0c0f5d12033829d9cb9f9dc40e6a538297d09a71
Steven Blair <stevenblair44@gmail.com>
2019-03-20 21:02:41 -0500

    Initial Commit

---
+++ ClickerMonkey3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClickerMonkey3
+//
+//  Created by Steven Blair on 3/20/19.
+//  Copyright © 2019 Steven Blair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
